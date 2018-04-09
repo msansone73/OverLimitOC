0015e3e0895686bdb6e4b9f572f5da0d0ce64c54
root <msansone@gmail.com>
2018-04-09 16:45:46 +0100

    atualizcao dos shell scripts

--- shDockerExecute.sh
+++ shDockerExecute.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d  --name $1 -v /saida:/saida python:smashbox2
+docker run -d  --name $1 -h $1  -v /saida:/saida python:smashbox2
---
+++ shDockerExecuteGroup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+re="^[0-9]+$"
+inicio=1
+if  [[ "$1" =~ $re ]]; 
+then
+    for (( i=$inicio; i<=$1; i++ ))
+    do
+       docker run -d  --name "exec$i"  -h "exec$i"  -v /saida:/saida python:smashbox2
+    done
+else
+   echo "ERROR: PARAMETER MUST BE AN INTEGER - $1"
+fi
